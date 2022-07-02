902c4ccf56a595e3e932c07bc2535021a18adcae
nishchit <nishchitpatel84@gmail.com>
2022-07-02 22:28:51 +0530

    add helper function and added mainMint,abi,walletHelper

--- src/Mint-Ens/MainMint.js
+++ src/Mint-Ens/MainMint.js
@@ -1,16 +1,92 @@
-import React from "react";
-import { FetchAccount,CheckWinEth,ConnectWallet  } from '../WalletHelper/WalletHelper'
+import React, { useEffect, useState } from "react";
+// import abis from "./abis.json"
+import contractAbi from '../Contract/abis.json'
+import { FetchAccount,CheckWinEth,SwitchNetwork,Contract,readOnlyProvider,writeProvider, getSigner  } from '../WalletHelper/WalletHelper'
+import { ethers } from "ethers";
 // const helper  = require('../WalletHelper/WalletHelper')
 const MainMint = () => {
 
-    FetchAccount().then(res=>{
-        console.log(res)
-    }).catch(err=>{
-        console.log(err)
-    })
+    const [provider ,setProivder] =useState(null)
+    const [defaultAcc,setDefaultAccount] = useState(null)
+    const [contract,setContracts] = useState(null)
+    const [signer,setSigner] = useState(null)
+    const [inputValue,setInputValue] = useState('');
+
+    const connectWallet = ()=>{
+        FetchAccount().then(res=>{
+            console.log(res)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+    const _setSigner = (tempP)=>{
+        let signer = getSigner(tempP);
+        setSigner(signer);
+    }
+
+    const swtich = ()=>{
+        SwitchNetwork().then(res=>{
+            console.log(res)
+        }).catch(err=>{
+            console.log(err)
+        }) 
+    }
+    const setProivders = ()=>{
+         writeProvider().then(res=>{
+            console.log(res)
+            setProivder(res)
+            _setSigner(res)
+        });
+       
+    }
+    useEffect(()=>{
+        if(provider && signer){
+            console.log(provider,"setting provider and contract")
+            setContract()
+        }
+    },[provider])
+
+    const setContract=()=>{
+        Contract("0x13331029a38bdace91e471b516c57be4303bb14f",
+        contractAbi,provider).then(res=>{
+            console.log(res)
+            setContracts(res)
+        })
+    }
+    const getPrice = async()=>{
+        let price = await contract.price(inputValue).then(res=>{
+            console.log(res,parseInt(res.toHexString(),16)/100000000000000000)
+            
+        })
+    }
+    const handleInput =(event)=>{
+        console.log(event.target.value)
+        setInputValue(event.target.value)
+    }
   return (
     <>
       <h2>Main Mint</h2>
+      <div onClick={connectWallet}>
+        <a >ConnectWallet</a>
+      </div>
+      <div onClick={setProivders}>
+        <a >setProvider</a>
+      </div>
+
+      <div onClick={setContract}>
+        <a >setContract</a>
+      </div>
+      <div>
+            <input type={'text'} value={inputValue} onChange={handleInput}/> 
+        </div>
+      <div onClick={getPrice}>
+        
+        <a >getPrice</a>
+      </div>
+      <div onClick={swtich}>
+        <a >add Polygon to Network</a>
+      </div>
+
     </>
   );
 };
--- src/WalletHelper/WalletHelper.js
+++ src/WalletHelper/WalletHelper.js
@@ -1,40 +1,76 @@
-import { ethers, utils } from 'ethers'
+import { ethers, utils } from "ethers";
+export const CheckWinEth = () => {
+  let checkPromise = new Promise((resolve, reject) => {
+    if (window.ethereum) {
+      resolve();
+    } else {
+      let err = "window ethereum not found";
+      reject(err);
+    }
+  });
 
-export const CheckWinEth = ()=>{
-   let checkPromise = new Promise ((resolve,reject)=>{
-        if(window.ethereum){
-            resolve()
-        }else{
-            let err =  "window ethereum not found"
-            reject(err)
-        }
-   })
+  return checkPromise;
+};
 
-   return checkPromise;
-}
+export const FetchAccount = async () => {
+  return CheckWinEth().then(() => {
+    return window.ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then((res) => {
+        let iCappedAccount = utils.getAddress(
+          ethers.utils.getIcapAddress(res[0])
+        );
 
-export const ConnectWallet = async()=>{
-    CheckWinEth().then(()=>{
+        return iCappedAccount;
+      });
+  });
+};
 
-    }).catch(err=>{
-        return new Error(err).message;
+export const readOnlyProvider = async() => {
+  return CheckWinEth().then(() => {
+    return new ethers.providers.JsonRpcProvider( "https://polygon-mainnet.g.alchemy.com/v2/WIeYyhZo4J_UI8A2AqOi3KvCGVTK_nM4" ||
+      process.env.React_APP_AlchemyKey
+    );
+  });
+};
+export const writeProvider =async () => {
+  return CheckWinEth().then(() => {
+    return new ethers.providers.Web3Provider(window.ethereum, "any");
+  });
+};
+export const Contract = async (contractAddress, Abi, provider) => {
+  return CheckWinEth().then(() => {
+    return new ethers.Contract(contractAddress, Abi, provider);
+  });
+};
+export const SwitchNetwork = async() => {
+    let chainId = ethers.utils.hexValue(137)
+  let network = [
+    {
+      chainId: "0x89" ,
+      chainName: "Polygon Mainnet",
+      nativeCurrency: {
+        name: "Matic",
+        symbol: "Matic",
+        decimals: 18,
+      }, 
+    //   main net : https://polygon-rpc.com/
+    // testnet : https://rpc-mumbai.maticvigil.com/
+
+    // mainnet BlockExplorer : https://polygonscan.com/
+    // testnet BlockExplorer : https://mumbai.polygonscan.com/
+      rpcUrls: ["https://polygon-rpc.com/"],
+      blockExplorerUrls: ["https://polygonscan.com/"],
+    },
+  ];
+  return CheckWinEth().then(() => {
+    return window.ethereum.request({method:"wallet_addEthereumChain",params:network}).then(()=>{
+        return true
+    }).catch(()=>{
+        return false
     })
+  });
+};
+export const getSigner = (provider)=>{
+    return provider.getSigner()
 }
-
-export const FetchAccount = async()=>{
-        return CheckWinEth().then(()=>{
-            return window.ethereum.request({method:"eth_requestAccounts"}).then(res=>{
-                let iCappedAccount = utils.getAddress(ethers.utils.getIcapAddress(res[0]))
-                
-                return iCappedAccount;
-                
-            }).catch(err=>{
-                return new Error(err).message;
-            })
-
-            
-        }).catch(err=>{
-            return new Error(err).message;
-        })
-}
-
